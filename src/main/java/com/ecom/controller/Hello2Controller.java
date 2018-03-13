0b33611ded7542c534a68589db60ba1f9e5495d0
Albertiy <YUANYUE963@outlook.com>
2018-03-13 12:06:30 +0800

    2018年3月13日12:05:42——更新资源文件测试3。

---
+++ src/main/java/com/ecom/controller/Hello2Controller.java
@@ -0,0 +1,25 @@
+package com.ecom.controller;
+
+import com.ecom.dao.VarietyDao;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+@Controller
+public class Hello2Controller {
+
+    @RequestMapping("/testorder")
+    public String testOrder(){
+        System.out.println("test order");
+        return "test_order";
+    }
+
+    @RequestMapping("/order_header")
+    public String orderHeader(){
+        return "order_header";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/test_order.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: YUAN YUE
+  Date: 2018/3/13
+  Time: 10:15
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+
+    <title>Title</title>
+</head>
+<body>
+<jsp:include page="order_header.jsp" flush="true"></jsp:include>
+<h2>未发货 | 已发货 | 已完成</h2>
+<jsp:include page="footer.jsp" flush="true"></jsp:include>
+</body>
+</html>
