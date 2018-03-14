2bd127b3df8e1e1a5717e7b4c131282031ae9911
Albertiy <YUANYUE963@outlook.com>
2018-03-14 21:00:40 +0800

    2018年3月14日20:59:23——继续testorder：需要实现的功能：从数据库读取、搜索、排序、分页、修改。

--- src/main/java/com/ecom/pojo/Express.java
+++ src/main/java/com/ecom/pojo/Express.java
@@ -3,6 +3,9 @@ package com.ecom.pojo;
 import com.google.gson.annotations.SerializedName;
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Component
 public class Express {
     private int eid;            //运单id
@@ -16,4 +19,68 @@ public class Express {
     private String sAddress;    //收货地址
     @SerializedName("ecompany")
     private String eCompany;    //快递公司
+
+    public Express() {
+        Date day=new Date();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("新的快递单|创建时间："+df.format(day));
+        eTime = df.format(day);
+        eCompany = "自己联系";
+    }
+
+    public int getEid() {
+        return eid;
+    }
+
+    public void setEid(int eid) {
+        this.eid = eid;
+    }
+
+    public int getOid() {
+        return oid;
+    }
+
+    public void setOid(int oid) {
+        this.oid = oid;
+    }
+
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public String geteTime() {
+        return eTime;
+    }
+
+    public void seteTime(String eTime) {
+        this.eTime = eTime;
+    }
+
+    public String geteAddress() {
+        return eAddress;
+    }
+
+    public void seteAddress(String eAddress) {
+        this.eAddress = eAddress;
+    }
+
+    public String getsAddress() {
+        return sAddress;
+    }
+
+    public void setsAddress(String sAddress) {
+        this.sAddress = sAddress;
+    }
+
+    public String geteCompany() {
+        return eCompany;
+    }
+
+    public void seteCompany(String eCompany) {
+        this.eCompany = eCompany;
+    }
 }
--- src/main/java/com/ecom/pojo/OrderData.java
+++ src/main/java/com/ecom/pojo/OrderData.java
@@ -13,4 +13,60 @@ public class OrderData {
     private int subTotal;
     private boolean returned;
     private boolean judged;
+
+    public OrderData() {
+        pcount = 1;
+        shopPrice = 1;
+        subTotal = 1;
+        returned = false;
+        judged = false;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public int getPcount() {
+        return pcount;
+    }
+
+    public void setPcount(int pcount) {
+        this.pcount = pcount;
+    }
+
+    public int getShopPrice() {
+        return shopPrice;
+    }
+
+    public void setShopPrice(int shopPrice) {
+        this.shopPrice = shopPrice;
+    }
+
+    public int getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(int subTotal) {
+        this.subTotal = subTotal;
+    }
+
+    public boolean isReturned() {
+        return returned;
+    }
+
+    public void setReturned(boolean returned) {
+        this.returned = returned;
+    }
+
+    public boolean isJudged() {
+        return judged;
+    }
+
+    public void setJudged(boolean judged) {
+        this.judged = judged;
+    }
 }
