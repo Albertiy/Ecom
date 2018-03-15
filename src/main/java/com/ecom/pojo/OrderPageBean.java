3201555b502afd8f6915b0aa968d3b9c349e788d
Albertiy <YUANYUE963@outlook.com>
2018-03-15 20:39:23 +0800

    2018年3月15日20:39:02——已完成订单搜索与分页的前端部分。开始连接数据库2。

--- src/main/java/com/ecom/pojo/OrderPageBean.java
+++ src/main/java/com/ecom/pojo/OrderPageBean.java
@@ -2,43 +2,92 @@ package com.ecom.pojo;
 
 import java.util.List;
 
-public class PageBean<T>{
-    private int currentPage;
-    private int currentCount;
-    private int totalCount;
-    private int totalPage;
+/*
+* 自定义:根据bootstrap-table参数定制的Bean，拥有搜索、单列排序以及分页功能。
+* @search:  搜索的内容
+* @sort:    排序的列名(只取第一个，使用MYSql的简单排序，例：select * from table order by value1 desc)
+* @order:   排序的顺序，有 asc（升序） 和 desc（降序） 两种
+* @total:   分页内容的总数目
+* @offset:  当前页的偏移值（不是页数）
+* @limit:   每页显示数目
+* @List:    当前页面的内容（总数不等于total，而是等于limit）
+* */
+public class OrderPageBean<T>{
+
+    private String search;
+    private String sort;
+    private String order;
+    private int total;
+    private int offset;
+    private int limit;
     private List<T> list;
 
-    public int getCurrentPage() {
-        return currentPage;
+    public OrderPageBean() {
+        search = "";
+        sort = "";
+        order = "asc";
+        total = 0;
+        offset = 0;
+        limit = 20;
+        list = null;
     }
 
-    public void setCurrentPage(int currentPage) {
-        this.currentPage = currentPage;
+    public OrderPageBean(String search, String sort, String order, int offset, int limit) {
+        this.search = search;
+        this.sort = sort;
+        this.order = order;
+        this.offset = offset;
+        this.limit = limit;
+        this.total = 0;
+        this.list = null;
     }
 
-    public int getCurrentCount() {
-        return currentCount;
+    public String getSearch() {
+        return search;
     }
 
-    public void setCurrentCount(int currentCount) {
-        this.currentCount = currentCount;
+    public void setSearch(String search) {
+        this.search = search;
     }
 
-    public int getTotalCount() {
-        return totalCount;
+    public String getSort() {
+        return sort;
     }
 
-    public void setTotalCount(int totalCount) {
-        this.totalCount = totalCount;
+    public void setSort(String sort) {
+        this.sort = sort;
     }
 
-    public int getTotalPage() {
-        return totalPage;
+    public String getOrder() {
+        return order;
     }
 
-    public void setTotalPage(int totalPage) {
-        this.totalPage = totalPage;
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
     }
 
     public List<T> getList() {
--- src/main/java/com/ecom/service/OrderService.java
+++ src/main/java/com/ecom/service/OrderService.java
@@ -1,4 +1,31 @@
 package com.ecom.service;
 
+import com.ecom.dao.OrderDao;
+import com.ecom.pojo.Order;
+import com.ecom.pojo.OrderPageBean;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.sql.SQLException;
+
+@Service
 public class OrderService {
+
+    @Resource(name = "orderDao")
+    private OrderDao orderDao;
+
+    public OrderPageBean<Order> findUnFilledOrdersBySid(int sid,OrderPageBean<Order> orderPageBean){
+        System.out.println("【OrderService：开始】");
+        try {
+            orderPageBean = orderDao.findUnfilledOrders(sid, orderPageBean);
+        }catch(SQLException se){
+            System.out.println(se);
+            System.out.println("【OrderService：失败】获取数据异常！");
+        }catch(Exception e){
+            System.out.println(e);
+            System.out.println("【OrderService：失败】");
+        }
+        System.out.println("【OrderService：成功】");
+        return orderPageBean;
+    }
 }
