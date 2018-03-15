ece3d36fad92fc4f449b2dd066dcededc89bdbd6
GustPig <245405027@qq.com>
2018-03-15 09:13:16 +0800

    ...

--- src/main/java/com/ecom/controller/GustPigController.java
+++ src/main/java/com/ecom/controller/GustPigController.java
@@ -5,5 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GustPigController {
-
+    @RequestMapping("/test_GustPig")
+    public String myStore(){
+        return "test_GustPig";
+    }
 }
